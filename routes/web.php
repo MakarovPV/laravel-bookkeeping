f59a00e6d65c3f3a3bc13cb77a5feea4251f3efe
MakarovPV <halarden1@gmail.com>
2022-11-25 17:17:57 +0500

    added authentication check

--- routes/web.php
+++ routes/web.php
@@ -12,24 +12,27 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['middleware' => 'auth.check'], function(){
+    Route::get('/', [App\Http\Controllers\RenderController::class, 'index'])->name('render.index');
 
-Route::get('/', [App\Http\Controllers\RenderController::class, 'index'])->name('render.index');
+    Route::prefix('income')->group(function(){
+        Route::post('/', [App\Http\Controllers\IncomeCategoriesController::class, 'store'])->name('income.categories.store');
+        Route::post('/update/{id}', [App\Http\Controllers\IncomeCategoriesController::class, 'update'])->name('income.categories.update');
+        Route::post('/delete/{id}', [App\Http\Controllers\IncomeCategoriesController::class, 'destroy'])->name('income.categories.destroy');
+    });
 
-Route::get('/build/income/', [App\Http\Controllers\Charts\IncomeChartController::class, 'sendDataForChart'])->name('build.income');
-Route::get('/build/expenses/', [App\Http\Controllers\Charts\ExpensesChartController::class, 'sendDataForChart'])->name('build.expenses');
-Route::get('/build/income_and_expenses', [App\Http\Controllers\Charts\IncomeExpensesCompareChartController::class, 'sendDataForChart'])->name('build.income_and_expenses');
+    Route::prefix('expenses')->group(function() {
+        Route::post('/', [App\Http\Controllers\ExpensesCategoriesController::class, 'store'])->name('expenses.categories.store');
+        Route::post('/update/{id}', [App\Http\Controllers\ExpensesCategoriesController::class, 'update'])->name('expenses.categories.update');
+        Route::post('/delete/{id}', [App\Http\Controllers\ExpensesCategoriesController::class, 'destroy'])->name('expenses.categories.destroy');
+    });
+});
 
-Route::post('/income', [App\Http\Controllers\IncomeCategoriesController::class, 'store'])->name('income.categories.store');
-Route::post('/income/update/{id}', [App\Http\Controllers\IncomeCategoriesController::class, 'update'])->name('income.categories.update');
-Route::post('/income/delete/{id}', [App\Http\Controllers\IncomeCategoriesController::class, 'destroy'])->name('income.categories.destroy');
-
-Route::post('/expenses', [App\Http\Controllers\ExpensesCategoriesController::class, 'store'])->name('expenses.categories.store');
-Route::post('/expenses/update/{id}', [App\Http\Controllers\ExpensesCategoriesController::class, 'update'])->name('expenses.categories.update');
-Route::post('/expenses/delete/{id}', [App\Http\Controllers\ExpensesCategoriesController::class, 'destroy'])->name('expenses.categories.destroy');
+Route::prefix('build')->group(function() {
+    Route::get('/income/', [App\Http\Controllers\Charts\IncomeChartController::class, 'sendDataForChart'])->name('build.income');
+    Route::get('/expenses/', [App\Http\Controllers\Charts\ExpensesChartController::class, 'sendDataForChart'])->name('build.expenses');
+    Route::get('/income_and_expenses', [App\Http\Controllers\Charts\IncomeExpensesCompareChartController::class, 'sendDataForChart'])->name('build.income_and_expenses');
+});
 
 Auth::routes();
-
-Route::get('/home', function () {
-    return redirect('/');
-});
 
